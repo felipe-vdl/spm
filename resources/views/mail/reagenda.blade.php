551c0888f76e3286ba1041333158358e5c52788d
Felipe Vidal <felipe.vidal.mesquita@gmail.com>
2022-05-13 13:03:56 -0300

    Reagendamento em Massa

--- resources/views/mail/reagenda.blade.php
+++ resources/views/mail/reagenda.blade.php
@@ -6,7 +6,7 @@
 
 <b>Página de Confirmação:</b> https://periciamedica.mesquita.rj.gov.br/confirmar
 
-<h3 style="margin-bottom: 5px;">Requerimento</h3>
+<h3 style="margin-bottom: 5px;">Requerimento Reagendado</h3>
 <ul style="margin-top: 0;">
     <li><b>Protocolo do seu requerimento:</b> {{ $requerimento->protocolo }}</li>
     <li><b>Direcionamento:</b> {{ $requerimento->direcionamento }}</li>
@@ -20,7 +20,7 @@
     </li>
 </ul>
 
-<p>* Atenção: Para confirmar a presença no dia e horário agendado o servidor precisa confirmar o recebimento deste e-mail, inserindo o protocolo do agendamento em nossa <a target="_blank" rel="noopener noreferrer" href="https://periciamedica.mesquita.rj.gov.br/confirmar">página de confirmação</a>.</p>
+<p>* Atenção: Para confirmar a presença no dia e horário reagendado o servidor precisa confirmar o recebimento deste e-mail, inserindo o protocolo do agendamento em nossa <a target="_blank" rel="noopener noreferrer" href="https://periciamedica.mesquita.rj.gov.br/confirmar">página de confirmação</a>.</p>
 
 <p>I - No dia e no horário agendado o servidor deverá estar munido do atestado do médico assistente ou odontológico original, contendo informações que constatem a incapacidade laborativa do servidor, conforme preconiza a resolução do Conselho Federal de Medicina nº 1851/2008 (D.O.U. 18/08/2008);</p>
 
