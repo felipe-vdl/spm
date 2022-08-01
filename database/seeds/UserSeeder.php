7cda4edf7a99e6fcfb7ef63dcffea00077add14a
Felipe Vidal <felipe.vidal.mesquita@gmail.com>
2022-08-01 16:57:38 -0300

    DB Seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -4,18 +4,44 @@ use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('users')->insert([
-            'name' => 'Felipe Emanoel',
-            'email' => 'felipe.vidal.mesquita@gmail.com',
-            'nivel' => 'Super-Admin',
-            'password' => bcrypt('pmm12345'),
-        ]);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    DB::table('users')->insert([
+      [
+        'name' => 'Felipe Emanoel',
+        'email' => 'felipe.vidal.mesquita@gmail.com',
+        'nivel' => 'Super-Admin',
+        'password' => bcrypt('pmm12345'),
+      ],
+      [
+        'name' => 'Victor Mussel',
+        'email' => 'victor.mussel@hotmail.com',
+        'nivel' => 'Super-Admin',
+        'password' => bcrypt('pmm12345'),
+      ],
+      [
+        'name' => 'Daniela da Silva Monteiro de Almeida',
+        'email' => 'danielamonteiro@mesquita.rj.gov.br',
+        'nivel' => 'Super-Admin',
+        'password' => bcrypt('pmm12345'),
+      ],
+      [
+        'name' => 'Keren Gomes de Lima',
+        'email' => 'pericia.medica@mesquita.rj.gov.br',
+        'nivel' => 'Admin',
+        'password' => bcrypt('pmm12345'),
+      ],
+      [
+        'name' => 'Priscila de Melo Santos Teixeira',
+        'email' => 'priscila.melo@mesquita.rj.gov.br',
+        'nivel' => 'Admin',
+        'password' => bcrypt('pmm12345'),
+      ],
+    ]);
+  }
 }
